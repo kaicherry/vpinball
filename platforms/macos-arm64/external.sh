7771df757fb263f7a84025a4bf2c18bc0bb7bb4d
kaicherry <kaicherry@mac.com>
2026-07-25 11:30:54 -0700

    Fix paths for openxr_loader64.dylib copy commands

--- platforms/macos-arm64/external.sh
+++ platforms/macos-arm64/external.sh
@@ -467,8 +467,8 @@ cp -a pinmame/pinmame/build/libpinmame.{dylib,*.dylib} ../../../third-party/runt
 cp pinmame/pinmame/src/libpinmame/libpinmame.h ../../../third-party/include
 
 #libopenxr_loader64.dylib -> openxr_loader64.dylib should probably be done here...
-cp openxr/openxr/build/src/loader/${BUILD_TYPE}/openxr_loader64.dylib ../../../third-party/build-libs/macos-arm64
-cp openxr/openxr/build/src/loader/${BUILD_TYPE}/openxr_loader64.dylib ../../../third-party/runtime-libs/windows-x64
+cp openxr/openxr/build/src/loader/openxr_loader64.dylib ../../../third-party/build-libs/macos-arm64
+cp openxr/openxr/build/src/loader/openxr_loader64.dylib ../../../third-party/runtime-libs/windows-x64
 cp -r openxr/openxr/include/openxr ../../../third-party/include
 
 cp -a libdmdutil/libdmdutil/build/libdmdutil.{dylib,*.dylib} ../../../third-party/runtime-libs/macos-arm64
